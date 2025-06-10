a968fa2ef23cd8586e5ca2e6b5e12478e2239a9a
Brandon Scott <brandonascott@live.ca>
2025-06-10 11:46:57 -0600

    Initial Commit

---
+++ Locked/LockedApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockedApp.swift
+//  Locked
+//
+//  Created by Brandon Scott on 2025-06-10.
+//
+
+import SwiftUI
+
+@main
+struct LockedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
