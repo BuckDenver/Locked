ba4e040d9f83e4c384e7dbe388d2af51ac61e24a
Brandon Scott <brandonascott@live.ca>
2025-06-12 15:13:55 -0600

    Working Version 1.0

---
+++ Locked/ProfileFormView.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileFormView.swift
+//  Locked
+//
+//  Created by Brandon Scott on 2025-06-11.
+//
+
+import Foundation
---
+++ Locked/ProfileManager.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileManager.swift
+//  Locked
+//
+//  Created by Brandon Scott on 2025-06-11.
+//
+
+import Foundation
